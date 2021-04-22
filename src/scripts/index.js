6816009f7c3daf0ea2ab9c4bc3d37cbf58105e93
Nikolaos P Argalias <nikosgalias@gmail.com>
2021-04-22 19:14:57 +0100

    hero completed

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,16 +1,11 @@
+// font awesome
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
 import '@fortawesome/fontawesome-free/js/regular';
 import '@fortawesome/fontawesome-free/js/brands';
+
+//scss
 import '../main.scss';
 
-document.querySelector('#burger').addEventListener('click', burgerMenu);
-
-function burgerMenu(e) {
-  console.log(e.target);
-  const navMenu = document.getElementById('menu__list');
-  navMenu.classList.toggle('menu__list--fade-in');
-  const burgerLines = document.getElementById('burger');
-  burgerLines.classList.toggle('burger__line--clicked');
-  console.log(navMenu);
-}
+//js
+import './menu/menu';
---
+++ src/scripts/menu/menu.js
@@ -0,0 +1,32 @@
+const publicEls = {
+  listItems: document.querySelectorAll('.menu__item'),
+  navMenu: document.getElementById('menu__nav'),
+  listItems: document.querySelectorAll('.menu__item'),
+  burgerBtn: document.getElementById('menu__burger-btn'),
+};
+
+// add transition delay to menu items so they slide one at a time
+publicEls.listItems.forEach((item, i) => {
+  item.style = `transition-delay: 0.${i + i}s`;
+});
+
+// burger btn click event listener
+publicEls.burgerBtn.addEventListener('click', onClickBurgerBtn);
+
+function onClickBurgerBtn(e) {
+  slideInMenu();
+  rotateBurgerLines();
+}
+
+function slideInMenu() {
+  const { navMenu, listItems } = publicEls;
+  navMenu.classList.toggle('menu__open');
+  listItems.forEach((item, i) => {
+    item.style = `transition-delay: 0.${i + i}s`;
+    item.classList.toggle('menu__open');
+  });
+}
+
+function rotateBurgerLines() {
+  publicEls.burgerBtn.classList.toggle('menu__burger-btn--clicked');
+}
