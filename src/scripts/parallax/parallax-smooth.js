ba6541662e0774f40c2189d855b35e2fa4de6372
Nikolaos P Argalias <nikosgalias@gmail.com>
2021-05-22 18:14:52 +0100

    promoted images to layers for GPU compositing

--- src/scripts/parallax/parallax-smooth.js
+++ src/scripts/parallax/parallax-smooth.js
@@ -1,84 +1,84 @@
-import throttle from '../performance/throttle';
+// import throttle from '../performance/throttle';
 
-window.addEventListener('DOMContentLoaded', (event) => {
-  // Hero Images
-  const imgOne = document.getElementById('para-img-1');
-  const imgTwo = document.getElementById('para-img-2');
-  const imgThree = document.getElementById('para-img-3');
-  const heroContent = document.getElementById('hero__content');
-  const hero__btn = document.getElementById('hero__btn');
-  const hero = document.getElementById('hero');
+// window.addEventListener('DOMContentLoaded', (event) => {
+//   // Hero Images
+//   const imgOne = document.getElementById('para-img-1');
+//   const imgTwo = document.getElementById('para-img-2');
+//   const imgThree = document.getElementById('para-img-3');
+//   const heroContent = document.getElementById('hero__content');
+//   const hero__btn = document.getElementById('hero__btn');
+//   const hero = document.getElementById('hero');
 
-  //   Portfolio Images & section
-  const stormTrooper = document.getElementById('storm-trooper-one');
-  const stormTrooper2 = document.getElementById('storm-trooper-two');
-  const stormTrooper3 = document.getElementById('storm-trooper-three');
-  const stormTrooper4 = document.getElementById('storm-trooper-four');
-  const portfolioSection = document.getElementById('portfolio');
+//   //   Portfolio Images & section
+//   const stormTrooper = document.getElementById('storm-trooper-one');
+//   const stormTrooper2 = document.getElementById('storm-trooper-two');
+//   const stormTrooper3 = document.getElementById('storm-trooper-three');
+//   const stormTrooper4 = document.getElementById('storm-trooper-four');
+//   const portfolioSection = document.getElementById('portfolio');
 
-  // About images
-  const aboutSection = document.getElementById('about-section');
-  const rebel1 = document.getElementById('rebel-one');
-  const rebel2 = document.getElementById('rebel-two');
+//   // About images
+//   const aboutSection = document.getElementById('about-section');
+//   const rebel1 = document.getElementById('rebel-one');
+//   const rebel2 = document.getElementById('rebel-two');
 
-  const parallaxThrottle = throttle(parallaxSequence, 50);
+//   const parallaxThrottle = throttle(parallaxSequence, 50);
 
-  window.addEventListener('scroll', parallaxThrottle);
+//   window.addEventListener('scroll', parallaxThrottle);
 
-  function parallaxSequence() {
-    if (isInViewport(hero)) {
-      heroParallax();
-    }
+//   function parallaxSequence() {
+//     if (isInViewport(hero)) {
+//       heroParallax();
+//     }
 
-    if (isInViewport(portfolioSection)) {
-      portfolioParallax();
-    }
+//     if (isInViewport(portfolioSection)) {
+//       portfolioParallax();
+//     }
 
-    if (isInViewport(aboutSection)) {
-      aboutParallax();
-    }
-  }
+//     if (isInViewport(aboutSection)) {
+//       aboutParallax();
+//     }
+//   }
 
-  function isInViewport(element) {
-    const elementPos = element.getBoundingClientRect();
-    if (elementPos.bottom >= elementPos.bottom - elementPos.bottom * 2) {
-      if (elementPos.top <= elementPos.height) {
-        return true;
-      }
-    }
-    return false;
-  }
+//   function isInViewport(element) {
+//     const elementPos = element.getBoundingClientRect();
+//     if (elementPos.bottom >= elementPos.bottom - elementPos.bottom * 2) {
+//       if (elementPos.top <= elementPos.height) {
+//         return true;
+//       }
+//     }
+//     return false;
+//   }
 
-  function heroParallax() {
-    const scrollValueProper =
-      (hero.offsetWidth * window.scrollY) / hero.clientHeight;
-    imgOne.style.transform = `translateX(${scrollValueProper}px)`;
-    imgTwo.style.transform = `translateX(-${scrollValueProper}px)`;
-    heroContent.style.transform = `translateX(${scrollValueProper}px)`;
-    hero__btn.style.transform = `translateX(-${scrollValueProper}px)`;
+//   function heroParallax() {
+//     const scrollValueProper =
+//       (hero.offsetWidth * window.scrollY) / hero.clientHeight;
+//     imgOne.style.transform = `translateX(${scrollValueProper}px)`;
+//     imgTwo.style.transform = `translateX(-${scrollValueProper}px)`;
+//     heroContent.style.transform = `translateX(${scrollValueProper}px)`;
+//     hero__btn.style.transform = `translateX(-${scrollValueProper}px)`;
 
-  }
+//   }
 
-  function portfolioParallax() {
-    const widthPercent =
-      ((window.scrollY - hero.clientHeight) / portfolioSection.clientHeight) *
-      portfolioSection.offsetWidth;
+//   function portfolioParallax() {
+//     const widthPercent =
+//       ((window.scrollY - hero.clientHeight) / portfolioSection.clientHeight) *
+//       portfolioSection.offsetWidth;
 
-    const heightPercent =
-      ((window.scrollY - hero.clientHeight) / portfolioSection.clientHeight) *
-      portfolioSection.offsetHeight;
+//     const heightPercent =
+//       ((window.scrollY - hero.clientHeight) / portfolioSection.clientHeight) *
+//       portfolioSection.offsetHeight;
 
-    stormTrooper.style.transform = `translateX(${widthPercent}px)`;
-    stormTrooper2.style.transform = `translateX(${-widthPercent}px)`;
-    stormTrooper4.style.transform = `translateX(${-widthPercent}px)`;
-  }
+//     stormTrooper.style.transform = `translateX(${widthPercent}px)`;
+//     stormTrooper2.style.transform = `translateX(${-widthPercent}px)`;
+//     stormTrooper4.style.transform = `translateX(${-widthPercent}px)`;
+//   }
 
-  function aboutParallax() {
-    const widthPercent =
-      ((window.scrollY - hero.clientHeight * 2) / aboutSection.offsetHeight) *
-      aboutSection.offsetWidth;
+//   function aboutParallax() {
+//     const widthPercent =
+//       ((window.scrollY - hero.clientHeight * 2) / aboutSection.offsetHeight) *
+//       aboutSection.offsetWidth;
 
-    rebel1.style.transform = `translateX(${-widthPercent}px)`;
-    rebel2.style.transform = `translateX(${widthPercent}px)`;
-  }
-});
+//     rebel1.style.transform = `translateX(${-widthPercent}px)`;
+//     rebel2.style.transform = `translateX(${widthPercent}px)`;
+//   }
+// });
